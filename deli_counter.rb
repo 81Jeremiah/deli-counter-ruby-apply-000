56e37c16e1ef4c99444e4a1700bcbd4d414a2f25
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-02-25 17:55:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli == []
+puts "The line is currently empty."
+
+else
+  line =[]
+  
+end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push (name)
+  return "${name} is the #{katz_deli} number in line"
+end
